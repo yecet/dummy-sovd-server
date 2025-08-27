819d6f8f90eb12eb583a559cd6bea38963bb9b87
Yunus Emre <yecetinkaya01@gmail.com>
2025-08-27 15:14:04 +0300

    Add CORS and OpenAPI JSON for SOVD

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# console.sovd.io’dan gelen requestleri kabul et
+origins = [
+    "https://console.sovd.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/about")
 def about():
